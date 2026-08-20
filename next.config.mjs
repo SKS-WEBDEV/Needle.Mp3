768cc772661346e4f32490f5444603bd7f64b5b9
Sudhev <69240835+SKS-WEBDEV@users.noreply.github.com>
2026-08-20 15:35:04 +0530

    Add outputFileTracingIncludes for ffmpeg-static

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/ffmpeg-static/**/*'],
+  },
 };
 
 export default nextConfig;
